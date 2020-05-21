30c809919c7f44f8986d72594d71dff5a7c1b136
abdelp <juniorperezpy@gmail.com>
2020-05-21 13:09:48 -0400

    Add constraints to friendship table

--- db/migrate/20200521153937_create_friendships.rb
+++ db/migrate/20200521153937_create_friendships.rb
@@ -1,8 +1,11 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.references :user, foreign_key: true
-      t.string :status
+      t.references :user, foreign_key: true, null: false
+      t.string :status, "char(1)", null: false, default: 'p',
+              inclusion: { in: ['a', 'r', 'p'], 
+                          as: :trigger,
+                          message: "can't be anything else than a, r, p" }
 
       t.timestamps
     end 
