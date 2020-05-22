9172dfd5d91bee5dfa34965a2c1605b977d819ca
kwizl <karis.njoroge@gmail.com>
2020-05-22 19:24:20 +0300

    Prevent user from sending a friend request to themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,21 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.find_by_sql(["SELECT * FROM users WHERE id != ? ", current_user.id])
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
-
+  
+  def follow_user
+    user_id = params[:user_id]
+    if Friendship.create(user_id: current_user.id, friend_id: user_id)
+      flash[:success] = "Request Sent"
+    else
+      flash[:danger] = "Failure to send request"
+    end
+    redirect_to users_path
+  end
 end
