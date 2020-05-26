a1c2f7c1377cfac0fbe8585ad273b0fa29bb1ee7
abdelp <juniorperezpy@gmail.com>
2020-05-26 12:26:47 -0400

    Add trigger and uk to friendships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_195855) do
+ActiveRecord::Schema.define(version: 2020_05_26_162349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_195855) do
     t.datetime "updated_at", null: false
     t.bigint "friend_id"
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "friendship_uk", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
