41baec0bbaf146aefe682ff9357b0f3e81c201cb
kwizl <karis.njoroge@gmail.com>
2020-06-01 22:57:08 +0300

    Write capybara test for liking and disliking a post

---
+++ spec/features/likes_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Like, driver: :selenium_chrome, js: true do
+  let(:user) { User.first }
+
+  before(:each) do
+    login_as(user)
+  end
+
+  it 'should increment a like' do
+    visit root_path
+    
+    within('.post-liking') do
+      click_link('Like!')
+    end
+
+    expect(page).to have_content 'You liked a post.'
+ 
+    within('.post-liking') do
+      click_link('Dislike!')
+    end
+
+    sleep(3)
+    expect(page).to have_content 'You disliked a post'
+  end
+end
