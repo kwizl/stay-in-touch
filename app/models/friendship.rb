8a7b9000feb3dfbea48b7eaff6b446e5d96eb2b1
abdelp <juniorperezpy@gmail.com>
2020-06-03 12:01:17 -0400

    Add friendship with different users validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   validates_uniqueness_of :user_id, scope: :friend_id
+  validate :different_users?
 
   def self.invitations(current_user)
     find_by_sql(
@@ -10,4 +11,9 @@ class Friendship < ApplicationRecord
       WHERE f.status = 'p' AND f.friend_id = ?", current_user]
     )
   end
+
+  private
+    def different_users?
+      errors.add(:friend_id, "friend must be different") unless user_id != friend_id
+    end
 end
